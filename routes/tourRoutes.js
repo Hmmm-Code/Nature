58fb263e7b12f9e7ac06d084f05c5496c4edf8ce
“Khan” <skcbskcb0@gmail.com>
2022-08-18 21:23:38 +0400

    develop get all tours

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,12 +5,10 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
-router
-  .route('/top-5-cheap')
-  .get(tourController.aliasTopTours, tourController.getAllTours);
+// router.route('/top-5-cheap').get(tourController.getAllTours);
 
-router.route('/tour-stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+// router.route('/tour-stats').get(tourController.getTourStats);
+// router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
 
 router
   .route('/')
